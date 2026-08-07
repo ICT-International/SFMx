20018118eef742f109dc1a32611701915f4f39d6
Toby-ICT-International <toby@ictinternational.com.au>
2026-08-07 12:26:32 +1000

    Update SFMx-C_MQTT_.json-mode_headers_Std+calculated-params.js
    * Created "units" labels for variables that are applicable, formatted to accommodate the changes.

--- SFMx-C_MQTT_.json-mode_headers_Std+calculated-params.js
+++ SFMx-C_MQTT_.json-mode_headers_Std+calculated-params.js
@@ -43,47 +43,58 @@
   },
 	"uo": {
     "label": "uncorrected-outer",
-    "description": "uncorrected Outer heat-pulse velocity HPV (cm/hr)"
+    "description": "uncorrected Outer heat-pulse velocity HPV"
+	"units": "cm/hr"
   },
 	"ui": {
     "label": "uncorrected-inner",
-    "description": "uncorrected inner heat-pulse velocity HPV (cm/hr)"
+    "description": "uncorrected inner heat-pulse velocity HPV"
+	"units": "cm/hr"
   },
 	"bv": {
     "label": "battery-voltage",
-    "description": "internal battery voltage (V)"
+    "description": "internal battery voltage"
+	"units": "V"
   },
 	"bc": {
     "label": "battery-charge-current",
-    "description": "internal battery charge rate (mA)"
+    "description": "internal battery charge rate"
+	"units": "mA"
   },
 	"bt": {
     "label": "internal-battery-temp",
-    "description": "internal battery and enclosure temperature (°C)"
+    "description": "internal battery and enclosure temperature"
+	"units": "C"
   },
 	"ev": {
     "label": "external-power-supply-voltage",
-    "description": "external power supply voltage (V)"
+    "description": "external power supply voltage"
+	"units": "V"
   },
 	"ec": {
     "label": "external-power-supply-current",
-    "description": "external power supply current consumption (mA)"
+    "description": "external power supply current consumption"
+	"units": "mA"
   },
 	"mtdo": {
     "label": "max-temp-downstream-outer",
     "description": "maximum temperature downstream outer"
+	"units": "C"
   },
 	"mtuo": {
     "label": "max-temp-upstream-outer",
     "description": "maximum temperature upstream outer"
+	"units": "C"
   },
 	"rtdo": {
     "label": "rise-temp-downstream-outer",
     "description": "rise temperature downstream outer"
+	"units": "C"
   },
 	"rtuo": {
     "label": "rise-temp-upstream-outer",
     "description": "rise temperature upstream outer"
+	"units": "C"
   },
 	"ro": {
     "label": "ratio-outer",
@@ -92,18 +103,22 @@
 	"mtdi": {
     "label": "max-temp-downstream-inner",
     "description": "maximum temperature downstream inner"
+	"units": "C"
   },
 	"mtui": {
     "label": "max-temp-upstream-inner",
     "description": "maximum temperature upstream inner"
+	"units": "C"
   },
 	"rtdi": {
     "label": "rise-temp-downstream-inner",
     "description": "rise temperature upstream inner"
+	"units": "C"
   },
 	"rtui": {
     "label": "rise-temp-upstream-inner",
     "description": "rise temperature upstream inner"
+	"units": "C"
   },
 	"ri": {
     "label": "ratio-inner",
@@ -112,6 +127,7 @@
 	"pd": {
     "label": "pulse-duration",
     "description": "heater probe pulse duration"
+	"units": "s"
   }
 }
 
